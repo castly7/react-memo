82348d5109963027a01c1c1d02978e460ae6350a
castly7 <castly97@gmail.com>
2022-12-06 18:51:26 +0300

    useMemo

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react' ;
+
+function Header({number,increment}) {
+    console.log("Header Re-rendered!");
+    return  (
+    <div>Header - {number} 
+    <br/>
+    <br/>
+    <button onClick={increment} >Click</button>
+    </div>
+    )
+}
+
+export default React.memo(Header);
